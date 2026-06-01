d1a6f12dc8433a973bfe64b0a1b52290faffac64
K140024(KDB) <jjong@kdb.co.kr>
2026-06-01 17:37:44 +0900

    메타반영(code)

--- src/main/java/com/kdb/it/domain/log/entity/CcodemL.java
+++ src/main/java/com/kdb/it/domain/log/entity/CcodemL.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
  * 코드 마스터(TPRMPP_CCODEM) 변경 로그 엔티티.
  */
 @Entity
-@Table(name = "TPRMPP_CCODEL", comment = "코드 마스터 변경 로그")
+@Table(name = "TPRMPP_CCODEL", comment = "프로젝트관리_공통코드기본변경로그")
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
