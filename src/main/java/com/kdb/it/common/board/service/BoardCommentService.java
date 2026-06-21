4b5f0d315de6db48c012f6ff24105b05c7b9e9f7
K140024(DESKTOP) <K140024@kdb.co.kr>
2026-06-22 08:10:08 +0900

    refactor: remove unused LocalDate import in BoardCommentService

--- src/main/java/com/kdb/it/common/board/service/BoardCommentService.java
+++ src/main/java/com/kdb/it/common/board/service/BoardCommentService.java
@@ -19,7 +19,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
