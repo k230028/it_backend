91afd961cc66fa8df1558c7c9e736b41019ae099
K140024(KDB) <jjong@kdb.co.kr>
2026-08-24 22:58:41 +0900

    test(request): 2026 샘플 분류 기준 갱신

--- src/test/java/com/kdb/it/domain/migration/request/service/RequestForm2026SampleSmokeTest.java
+++ src/test/java/com/kdb/it/domain/migration/request/service/RequestForm2026SampleSmokeTest.java
@@ -153,8 +153,8 @@ class RequestForm2026SampleSmokeTest {
             if (classifySample(sampleRoot, path)) requestForms++;
         }
 
-        assertThat(excelFiles.size()).isEqualTo(44);
-        assertThat(requestForms).isEqualTo(32);
+        assertThat(excelFiles.size()).isEqualTo(49);
+        assertThat(requestForms).isEqualTo(37);
         assertThat(excelFiles.size() - requestForms).isEqualTo(12);
     }
 
